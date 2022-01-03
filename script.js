d4dc8c1667e4d7bf9ff11135dac59679f671bc0c
Himalaya <yugmani@yahoo.com>
2022-01-03 00:20:20 -0800

    javascript Done

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const colorBtn = document.querySelector(".colorBtn");
 const bodyBcg = document.querySelector("body");
+const colorName = document.querySelector("#colorName");
 
-const colors = ["yellow", "red", "green", "#3b5998"];
+const colors = [
+  "yellow",
+  "purple",
+  "red",
+  "green",
+  "blue",
+  "silver",
+  "gold",
+  "bisque",
+  "cyan",
+  "#3b5998",
+];
 
 colorBtn.addEventListener("click", changeColor);
 
-function changeColor() {}
+function changeColor() {
+  let random = Math.floor(Math.random() * colors.length);
+  bodyBcg.style.backgroundColor = colors[random];
+  colorName.innerText = colors[random];
+  colorName.style.backgroundColor = colors[random];
+}
