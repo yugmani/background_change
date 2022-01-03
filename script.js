91a16d5cf82877fa009238ee21cf65049de959a8
Himalaya <yugmani@yahoo.com>
2022-01-02 23:57:05 -0800

    html css Done

---
+++ script.js
@@ -0,0 +1,8 @@
+const colorBtn = document.querySelector(".colorBtn");
+const bodyBcg = document.querySelector("body");
+
+const colors = ["yellow", "red", "green", "#3b5998"];
+
+colorBtn.addEventListener("click", changeColor);
+
+function changeColor() {}
